3a025197adfe5f35b9513b7d610056ea551c3617
anicmyk <aniq.bnbecomm@gmail.com>
2026-04-11 21:28:43 +0500

    fix trailing slash url style

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://itsmypdf.com',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
   integrations: [
     react(),
     tailwind(),
     sitemap({
       filter: (page) =>
+        page !== 'https://itsmypdf.com/privacy' &&
         page !== 'https://itsmypdf.com/privacy/' &&
+        page !== 'https://itsmypdf.com/terms' &&
         page !== 'https://itsmypdf.com/terms/'
     })
   ],
