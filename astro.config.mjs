defa38a8c6763645c404dca8ac249b466b6dddb5
anicmyk <aniq.bnbecomm@gmail.com>
2026-04-12 09:45:17 +0500

    chore: simplify sitemap setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://itsmypdf.com',
@@ -11,13 +10,6 @@ export default defineConfig({
   },
   integrations: [
     react(),
-    tailwind(),
-    sitemap({
-      filter: (page) =>
-        page !== 'https://itsmypdf.com/privacy' &&
-        page !== 'https://itsmypdf.com/privacy/' &&
-        page !== 'https://itsmypdf.com/terms' &&
-        page !== 'https://itsmypdf.com/terms/'
-    })
+    tailwind()
   ],
 });
